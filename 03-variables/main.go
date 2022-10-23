6792f1efc511635f6fd297b9ada56866ec336b15
fedemza <fedevoloschin@gmail.com>
2022-10-23 12:33:36 -0300

    variables

---
+++ 03-variables/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hola mundo")
+
+	var mes string
+
+	mes = "enero"
+
+	var mes2 string = "febrero"
+
+	mes3 := "marzo"
+
+	fmt.Println(mes, mes2, mes3)
+
+	numero1 := 6
+
+	numero2 := 8
+
+	suma := numero1 + numero2
+
+	fmt.Println(suma)
+
+	var correcto bool
+
+	var nombre string
+
+	var num float64
+
+	fmt.Println(correcto, nombre, num)
+}
